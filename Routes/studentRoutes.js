6a32c3433e0118edf41b908ae5e91d1263facfb6
George-Clifford <georgecliffordodiwuor@gmail.com>
2024-03-19 07:55:37 +0300

    added the GET, PATCH, update students and get students by id

--- Routes/studentRoutes.js
+++ Routes/studentRoutes.js
@@ -3,6 +3,10 @@ const studentController = require ("../controller/studentController");
 const  router=express.Router();
 
 router.post("/addStudent",studentController.addStudent);
+router.get("/getAllStudents", studentController.getAllStudents);
+router.get("/getStudent/:student_id", studentController.getStudent);
+router.patch("/updateStudent/:id", studentController.updateStudent);
+
 
 
 module.exports=router;
