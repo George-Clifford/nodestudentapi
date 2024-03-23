6257dcfee72eeb56bf09e87637518cb566b23277
George-Clifford <georgecliffordodiwuor@gmail.com>
2024-03-23 15:08:54 +0300

    debugged the code to generate access tokens

--- controller/registrationController.js
+++ controller/registrationController.js
@@ -1,36 +1,70 @@
-const db = require('../Model/dbConnect')
-const createError = require('http-errors')
+// const db = require('../Model/dbConnect')
+// const createError = require('http-errors')
+// const {authSchema} = require('../helpers/validateSchema')
 
-const { signAccessToken } = require('../helpers/jwthelpers')
+// const { signAccessToken } = require('../helpers/jwthelpers')
 
-const user = db.users
+// const user = db.users
+
+// module.exports = {
+//     addUser : async(req, res, next)=> {
+//         try{
+//             const {email, password}= await authSchema.validateAsync(req.body)
+//             const exists = await user.findOne({where: {email}})
+//             if (exists) {
+//                 throw createError.Conflict('${email} has already been registered')
+//             }
+//             const newUser = new user({email, password});
+//             const savedUser = await newUser.save();
+
+//             const accessToken = await signAccessToken(savedUser.user_id);
+//             res.status(200).send({accessToken})
+//         } catch(error) {
+//             next(error)
+//         }
+        
+// },
+//     getAllUsers:(req,res,next)=>{
+//         try{
+//             let user = user.findAll({})
+//             res.status(200).send(user)
+//         }
+//         catch (error)
+//         {
+//             next(error)
+//         }
+// },
+// }
+const db = require('../Model/dbConnect');
+const createError = require('http-errors');
+const { authSchema } = require('../helpers/validateSchema');
+const { signAccessToken } = require('../helpers/jwthelpers');
+
+const User = db.users;
 
 module.exports = {
-    addUser : async(req, res, next)=> {
-        try{
-            const {email, password}= await authSchema.validateAsync(req.body)
-            const exists = await user.findOne({where: {email}})
-            if (exists) {
-                throw createError.Conflict('${email} has already been registered')
-            }
-            const newUser = new user({email, password});
-            const savedUser = await newUser.save();
+  addUser: async (req, res, next) => {
+    try {
+      const { email, password } = await authSchema.validateAsync(req.body);
+      const exists = await User.findOne({ where: { email } });
+      if (exists) {
+        throw createError.Conflict(`${email} has already been registered`);
+      }
+      const newUser = new User({ email, password });
+      const savedUser = await newUser.save();
 
-            const accessToken = await signAccessToken(savedUser.user_id);
-            res.status(200).send({accessToken})
-        } catch(error) {
-            next(error)
-        }
-        
-},
-    getAllUsers:(req,res,next)=>{
-        try{
-            let user = user.findAll({})
-            res.status(200).send(user)
-        }
-        catch (error)
-        {
-            next(error)
-        }
-},
-}
+      const accessToken = await signAccessToken(savedUser.id); // Assuming ID is accessible directly
+      res.status(200).send({ accessToken });
+    } catch (error) {
+      next(error);
+    }
+  },
+  getAllUsers: async (req, res, next) => {
+    try {
+      const users = await User.findAll({});
+      res.status(200).send(users);
+    } catch (error) {
+      next(error);
+    }
+  },
+};
--- helpers/jwthelpers.js
+++ helpers/jwthelpers.js
@@ -1,71 +1,141 @@
+// const createError = require('http-errors');
+// const JWT = require('jsonwebtoken');
+// //const student = require('../Model/registrationModel');
+
+// module.exports = {
+//     signAccessToken: (UserId)=>{
+//         return new Promise ((resolve, reject)=>{
+//             const payload = {UserId}
+//             const secret = process.env.ACCCESS_TOKEN_SECRET;
+//             const options ={
+//                 expiresIn:'10m',
+//                 issuer:'Clifford.com',
+//                 audience: 'UserId.toString'(),
+//             }
+//             JWT.sign(payload, secret, options,(error, token)=>{
+//                 if(error) {
+//                     console.log(error.message)
+//                     reject(createError.InternalServerError());
+//                 }
+//                 resolve(token);
+//             })
+//         });
+        
+//     },
+
+//     verifyAccessToken:(req, res, next)=> {
+//         if(!req.headers['authorization']) return next (createError.unauthorized())
+//         const authHeader = req.headers['authorization']
+//         const bearerToken = authHeader.split('')
+//         const token = bearerToken[1]
+//         JWT.verify(token, process.env.ACCCESS_TOKEN_SECRET, (err, payload)=>{
+//             if(err) {
+//                 if(err.name ==='jsonwebtoken error'){
+//                     return next(createError.unauthorized(error.message))
+//                 }
+//             }
+//             req.payload = payload
+//             next()
+//         })
+//     },
+
+//     signRefreshToken:(UserId)=>{
+//         return new Promise((resolve, reject)=>{
+//             const payload = {}
+//             const secret = process.env.REFRESH_TOKEN_SECRET;
+//             const options ={
+//                 expiresIn: '1y',
+//                 issuer: 'Clifford.com',
+//                 audience: UserId.toString
+//             }
+//             JWT.sign(payload, secret, options, (error, token)=>{
+//                 if(error) {
+//                     console.log (error.message)
+//                     reject(createError.InternalServerError());
+//                 }
+//                 resolve(token);
+//             })
+//         })
+        
+//     },
+
+//     verifyRefreshToken: (refreshToken)=>{
+//         return new Promise ((resolve, reject)=> {
+//             JWT.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, payload)=>{
+//                 if(err) return reject(createError.unauthorized())
+//                 const UserId = payload.aud
+//             resolve(UserId.toString())
+//             })
+//         })
+//     },
+// }
 const createError = require('http-errors');
 const JWT = require('jsonwebtoken');
-//const student = require('../Model/registrationModel');
 
 module.exports = {
-    signAccessToken: (UserId)=>{
-        return new Promise ((resolve, reject)=>{
-            const payload = {UserId}
-            const secret = process.env.ACCCESS_TOKEN_SECRET;
-            const options ={
-                expiresIn:'10m',
-                issuer:'Clifford.com',
-                audience: 'UserId.toString'(),
-            }
-            JWT.sign(payload, secret, options,(error, token)=>{
-                if(error) {
-                    console.log(erro.message)
-                    reject(createError.InternalServerError());
-                }
-                resolve(token);
-            })
-        });
-        
-    },
+  signAccessToken: (userId) => {
+    return new Promise((resolve, reject) => {
+      const payload = { userId };
+      const secret = process.env.ACCESS_TOKEN_SECRET;
+      const options = {
+        expiresIn: '10m',
+        issuer: 'Clifford.com',
+        audience: userId.toString(), // Corrected here
+      };
+      JWT.sign(payload, secret, options, (error, token) => {
+        if (error) {
+          console.log(error.message);
+          reject(createError.InternalServerError());
+        }
+        resolve(token);
+      });
+    });
+  },
 
-    verifyAccessToken:(req, res, next)=> {
-        if(!req.headers['authorization']) return next (createError.unauthorized())
-        const authHeader = req.headers['authorization']
-        const bearerToken = authHeader.split('')
-        const token = bearerToken[1]
-        JWT.verify(token, process.env.ACCCESS_TOKEN_SECRET, (err, payload)=>{
-            if(err) {
-                if(err.name ==='jsonwebtoken error'){
-                    return next(createError.unauthorized(error.message))
-                }
-            }
-            req.payload = payload
-            next()
-        })
-    },
+  verifyAccessToken: (req, res, next) => {
+    if (!req.headers['authorization']) return next(createError.Unauthorized());
+    const authHeader = req.headers['authorization'];
+    const bearerToken = authHeader.split(' ');
+    const token = bearerToken[1];
+    JWT.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
+      if (err) {
+        if (err.name === 'JsonWebTokenError') {
+          return next(createError.Unauthorized(err.message));
+        } else {
+          return next(createError.Unauthorized());
+        }
+      }
+      req.payload = payload;
+      next();
+    });
+  },
 
-    signRefreshToken:(UserId)=>{
-        return new Promise((resolve, reject)=>{
-            const payload = {}
-            const secret = process.env.REFRESH_TOKEN_SECRET;
-            const options ={
-                expiresIn: '1y',
-                issuer: 'Clifford.com',
-                audience: UserId.toString
-            }
-            JWT.sign(payload, secret, options, (error, token)=>{
-                if(error) {
-                    console.log (error.message)
-                    reject(createError.InternalServerError());
-                }
-                resolve(token);
-            })
-        })
-        
-    },
+  signRefreshToken: (userId) => {
+    return new Promise((resolve, reject) => {
+      const payload = {};
+      const secret = process.env.REFRESH_TOKEN_SECRET;
+      const options = {
+        expiresIn: '1y',
+        issuer: 'Clifford.com',
+        audience: userId.toString(), // Corrected here
+      };
+      JWT.sign(payload, secret, options, (error, token) => {
+        if (error) {
+          console.log(error.message);
+          reject(createError.InternalServerError());
+        }
+        resolve(token);
+      });
+    });
+  },
 
-    verifyRefreshToken: (refreshToken)=>{
-        return new Promise ((resolve, reject)=> {
-            JWT.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, payload)=>{
-                if(err) return reject(createError.unauthorized())
-                const UserId = payload.aud
-            resolve(UserId.toString())
-            })
-        })
-    },
-}
+  verifyRefreshToken: (refreshToken) => {
+    return new Promise((resolve, reject) => {
+      JWT.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, payload) => {
+        if (err) return reject(createError.Unauthorized());
+        const userId = payload.aud;
+        resolve(userId.toString());
+      });
+    });
+  },
+};
